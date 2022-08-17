de7623e965aa912ebd9c66d090dbaf884cbfd2f4
asiorgad <asiorgad@gmail.com>
2022-08-17 23:59:25 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,3 @@
 def run_me():
   print('hello world')
-  from machine import Pin
-  # create an output pin on pin #0
-  p0 = Pin(0, Pin.OUT)
-
-  # set the value low then high
-  p0.value(0)
-  p0.value(1)
-
-  # create an input pin on pin #2, with a pull up resistor
-  p2 = Pin(2, Pin.IN, Pin.PULL_UP)
+  print('hello world111223234234')
